98536162bd76879b3cf5b1c9ccacb2c84d46df0a
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-01-21 22:27:58 +0100

    Implement backpropagation

--- 4-neural-network/backprop.m
+++ 4-neural-network/backprop.m
@@ -1,39 +1,55 @@
-function [hidlw outlw terr] = backprop(tset, tslb, inihidlw, inioutlw, lr)
+function [hidden_layer output_layer total_error] = backprop(input, labels, inihidlw, inioutlw, lr)
 % derivative of sigmoid activation function
-% tset - training set (every row represents a sample)
-% tslb - column vector of labels
+% input - training set (every row represents a sample)
+% labels - column vector of labels
 % inihidlw - initial hidden layer weight matrix
 % inioutlw - initial output layer weight matrix
 % lr - learning rate
 
-% hidlw - hidden layer weight matrix
-% outlw - output layer weight matrix
-% terr - total squared error of the ANN
+% hidden_layer - hidden layer weight matrix
+% output_layer - output layer weight matrix
+% total_error - total squared error of the ANN
 
-% 1. Set output matrices to initial values
-	hidlw = inihidlw;
-	outlw = inioutlw;
+  % 1. Set output matrices to initial values
+  hidden_layer = inihidlw;
+  output_layer = inioutlw;
 
-% 2. Set total error to 0
-	terr = 0;
+  % 2. Set total error to 0
+  total_error = 0;
 
-% foreach sample in the training set
-	for i=1:rows(tset)
-		% 3. Set desired output of the ANN
-		% dout = -1 * ones(1, columns(outlw));
+  % foreach sample in the training set
+  for i=1:rows(input)
+    % 3. Set desired output of the ANN
+    desired_output = zeros(1, columns(output_layer));
+    desired_output(labels(i)) = 1;
+    desired_output;
 
-		% 4. Propagate input forward through the ANN
-		% remember to extend input [tset(i, :) 1]
+    % 4. Propagate input forward through the ANN
+    % remember to extend input [input(i, :) 1]
+    output_from_hidden = actf([input(i, :) 1] * hidden_layer);
+    output_from_output = actf([output_from_hidden 1] * output_layer);
 
-		% 5. Adjust total error (just to know this value)
+    % 5. Adjust total error (just to know this value)
+    sample_err = sum((desired_output - output_from_output) .^2);
+    total_error += sample_err;
 
-		% 6. Compute delta error of the output layer
-		% how many delta errors should be computed here?
+    % 6. Compute delta error of the output layer
+    % how many delta errors should be computed here?
+    delta_out = (output_from_output - desired_output) .* output_from_output .* (1 - output_from_output);
 
-		% 7. Compute delta error of the hidden layer
-		% how many delta errors should be computed here?
+    delta_out_w = output_from_hidden' * delta_out;
+    delta_out_b = delta_out;
 
-		% 8. Update output layer weights
+    % 7. Compute delta error of the hidden layer
+    % how many delta errors should be computed here?
+    delta_hid = delta_out * output_layer(1:end-1,:)' .* output_from_hidden .* (1 - output_from_hidden);
 
-		% 9. Update hidden layer weights
-	end
+    delta_hid_w = input(i,:)' * delta_hid;
+    delta_hid_b = delta_hid;
+
+    % 8. Update output layer weights
+    output_layer -= lr * [delta_out_w; delta_out_b];
+
+    % 9. Update hidden layer weights
+    hidden_layer -= lr * [delta_hid_w; delta_hid_b];
+  end
--- 4-neural-network/mainscript.m
+++ 4-neural-network/mainscript.m
@@ -16,26 +16,36 @@ tstv = tstv(:, toRemove);
 %	classifier function itself
 
 x = -5:0.1:5;
-plot(x, actf(x));
+%plot(x, actf(x));
 
-[hidl outl] = crann(2, 3, 2)
+[hidl outl] = crann(2, 3, 2);
 
 % xor data set
 tset = [-1 1; 1 1; 1 -1; -1 -1];
 tslb = [1; 2; 1; 2];
 
+tset = repmat(tset, 50, 1);
+tslb = repmat(tslb, 50, 1);
+
 res = anncls(tset, hidl, outl);
 cmx = confMx(tslb, res)
-compErrors(cmx)
+result = compErrors(cmx)
 
 % classifier is ready; now time for training
 % we'll need:
 %	derivative of the activation function
 %	backprop function which performs iterative backpropagation training
 
-plot(x, actdf(x));
+%plot(x, actdf(x));
 
-%[hhidl ooutl ter] = backprop(tset, tslb, hidl, outl, 0.1);
-%res = anncls(tset, hhidl, ooutl);
-%cmx = confMx(tslb, res)
-%compErrors(cmx)
+hhidl = hidl;
+ooutl = outl;
+
+for i=1:20
+  [hhidl ooutl ter] = backprop(tset, tslb, hhidl, ooutl, 0.5);
+end
+hidl(1:3, :)
+hhidl(1:3, :)
+res = anncls(tset, hhidl, ooutl);
+cmx = confMx(tslb, res)
+result = compErrors(cmx)
